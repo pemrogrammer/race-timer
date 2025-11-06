2259f5d02ca0c9088a68b1173a8dc2ff2ce24d39
Zain🍕 <zainadam.id@gmail.com>
2025-11-06 23:29:39 +0800

    Aligns app name with package version
    Updates the default `compName` setting to dynamically fetch the application version from `package.json`. This ensures the displayed application name consistently reflects the project's current version, improving maintainability and reducing manual update efforts.
    
    Also adds a missing semicolon for code consistency.

--- src/statics/default-settings.ts
+++ src/statics/default-settings.ts
@@ -1,10 +1,12 @@
+import packageJson from "../../package.json";
+
 /**
  * Default Settings
  */
 const DEFAULT_SETTINGS = {
     prep_time: 5, //second
     race_duration: 15, //second
-    compName: "Race Stopwatch App V1.9 ",
+    compName: `Race Stopwatch App v${packageJson.version}`,
     team_a_name: "Tim A",
     team_b_name: "Tim B",
     midText: "VS / Laps / Checkpoint",
@@ -13,4 +15,4 @@ const DEFAULT_SETTINGS = {
     isSfxEnabled: true,
 };
 
-export default DEFAULT_SETTINGS
+export default DEFAULT_SETTINGS;
